a1d16abb570462a90aad7b002b4ff8bf1c44cad1
Sonia <soniangaruiya@gmail.com>
2026-09-06 17:49:39 +0300

    Refactor User and Recipe models with SerializerMixin

--- server/models.py
+++ server/models.py
@@ -1,20 +1,22 @@
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.hybrid import hybrid_property
-from marshmallow import Schema, fields
+from sqlalchemy_serializer import SerializerMixin
 
 from config import db, bcrypt
 
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
+    serialize_rules = ('-recipes.user', '-_password_hash',)
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False, unique=True)
+    username = db.Column(db.String, unique=True, nullable=False)
     _password_hash = db.Column(db.String)
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
 
-    recipes = db.relationship('Recipe', backref='user')
+    recipes = db.relationship('Recipe', back_populates='user')
 
     @hybrid_property
     def password_hash(self):
@@ -30,12 +32,20 @@ class User(db.Model):
         return bcrypt.check_password_hash(
             self._password_hash, password.encode('utf-8'))
 
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError('Username must be present.')
+        return username
+
     def __repr__(self):
         return f'<User {self.username}>'
 
 
-class Recipe(db.Model):
+class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+
+    serialize_rules = ('-user.recipes',)
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -43,9 +53,10 @@ class Recipe(db.Model):
     minutes_to_complete = db.Column(db.Integer)
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', back_populates='recipes')
 
     @validates('instructions')
-    def validates_instructions(self, key, instructions):
+    def validate_instructions(self, key, instructions):
         if not instructions or len(instructions) < 50:
             raise ValueError(
                 'Instructions must be present and at least 50 characters long.')
@@ -53,18 +64,3 @@ class Recipe(db.Model):
 
     def __repr__(self):
         return f'<Recipe {self.title}>'
-
-
-class UserSchema(Schema):
-    id = fields.Integer()
-    username = fields.String()
-    image_url = fields.String()
-    bio = fields.String()
-
-
-class RecipeSchema(Schema):
-    id = fields.Integer()
-    title = fields.String()
-    instructions = fields.String()
-    minutes_to_complete = fields.Integer()
-    user = fields.Nested(UserSchema)
